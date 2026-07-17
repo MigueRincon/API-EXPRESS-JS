80418fd28b6a0899eb99b9ee260292d73e107876
MigueRincon <rinrincon527@gmail.com>
2026-07-17 17:22:08 -0500

    Rutas de productos y usuarios listas

--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,8 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const ctrl = require('../controllers/productController')
+const express = require('express');
+const router = express.Router();
+const {
+    obtenerProductos,
+    obtenerProductosPorCategoria,
+    obtenerProductosPorId,
+    crearProducto,
+    actualizarProducto,
+    eliminarProducto
+} = require('../controllers/productController');
 
-router.get('/', ctrl.obtenerTodos)   // GET /productos
-router.post('/', ctrl.crearProducto) // POST /productos
+// GET - todos los productos
+router.get('/', obtenerProductos);
 
-module.exports = router
+// GET - Obtener un producto por categoria
+router.get('/categoria/:categoria', obtenerProductosPorCategoria);
+
+// GET - Obtener un producto por ID
+router.get('/:id', obtenerProductosPorId);
+
+// POST - Crear un nuevo producto
+router.post('/', crearProducto);
+
+// PUT - Actualizar un producto
+router.put('/:id', actualizarProducto);
+
+// DELETE - Eliminar un producto
+router.delete('/:id', eliminarProducto);
+
+module.exports = router;
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const ctrl = require('../controllers/userController')
+const express = require('express');
+const router = express.Router();
+const {
+    obtenerUsuarios,
+    obtenerUsuariosPorId,
+    crearUsuario,
+    atualizarUsuario,
+    eliminarUsuario
+} = require('../controllers/userController');
 
-router.get('/', ctrl.obtenerUsuarios)
-router.post('/', ctrl.crearUsuario)
+//GET - Obtener todos los usuarios 
+router.get('/', obtenerUsuarios);
+
+// GET - Obtener un usuario por ID
+router.get('/:id', obtenerUsuarioPorId);
+
+// POST - Crear un nuevo usuario
+router.post('/',crearUsuario);
+
+// Put - Actualizar un usuario
+router.put('/:id', actualizarUsuario);
+
+// DELETE - Eliminar un usuario
+router.delete('/:id', eliminarUsuario);
 
 module.exports = router;
