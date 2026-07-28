2c09060d770cdb97d82b0116d2fc1df4fb7dacf7
MigueRincon <rinrincon527@gmail.com>
2026-07-28 14:17:19 +0000

    LISTO

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 // Función auxiliar: generar un token JWT
@@ -57,7 +57,7 @@ exports.registrar = async (req, res) => {
       error: error.message
     });
   }
-}; 
+};
 
 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registrar } = require('../controllers/authController');
+const { registrar, login } = require('../controllers/authController');
 
 // POST - Registrar nuevo usuario
 router.post('/register', registrar);
-router.post('/login', login);   // ← nueva ruta
+router.post('/login', login);
 
 module.exports = router;
