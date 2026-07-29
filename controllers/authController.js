8698bbb92ab68a68872100151e3a31b14c10a427
MigueRincon <rinrincon527@gmail.com>
2026-07-29 17:11:37 +0000

    OKI

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 // Función auxiliar: generar un token JWT
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Middleware: proteger rutas (verifica el token)
 exports.proteger = async (req, res, next) => {
