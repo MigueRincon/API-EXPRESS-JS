1d9e4f2d785052bc61c3da907b3343e98cdf8ca8
Miguel_Rincon <rinrincon527@gmail.com>
2026-07-30 19:23:41 -0500

    RICH POWER ANGULAR COMPONENTES

--- Rich-Power/src/app/app.ts
+++ Rich-Power/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Saludo } from './components/saludo/saludo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Saludo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
